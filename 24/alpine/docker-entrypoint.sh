873ff12ab29cf663accbb093f6d1959dbfa8bf89
gruve-p <jackielove4u@hotmail.com>
2023-06-10 16:28:36 +0200

    groestlize

--- 24/alpine/docker-entrypoint.sh
+++ 24/alpine/docker-entrypoint.sh
@@ -2,37 +2,37 @@
 set -e
 
 if [ -n "${UID+x}" ] && [ "${UID}" != "0" ]; then
-  usermod -u "$UID" bitcoin
+  usermod -u "$UID" groestlcoin
 fi
 
 if [ -n "${GID+x}" ] && [ "${GID}" != "0" ]; then
-  groupmod -g "$GID" bitcoin
+  groupmod -g "$GID" groestlcoin
 fi
 
-echo "$0: assuming uid:gid for bitcoin:bitcoin of $(id -u bitcoin):$(id -g bitcoin)"
+echo "$0: assuming uid:gid for groestlcoin:groestlcoin of $(id -u groestlcoin):$(id -g groestlcoin)"
 
 if [ $(echo "$1" | cut -c1) = "-" ]; then
-  echo "$0: assuming arguments for bitcoind"
+  echo "$0: assuming arguments for groestlcoind"
 
-  set -- bitcoind "$@"
+  set -- groestlcoind "$@"
 fi
 
-if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "bitcoind" ]; then
-  mkdir -p "$BITCOIN_DATA"
-  chmod 700 "$BITCOIN_DATA"
+if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "groestlcoind" ]; then
+  mkdir -p "$GROESTLCOIN_DATA"
+  chmod 700 "$GROESTLCOIN_DATA"
   # Fix permissions for home dir.
-  chown -R bitcoin:bitcoin "$(getent passwd bitcoin | cut -d: -f6)"
-  # Fix permissions for bitcoin data dir.
-  chown -R bitcoin:bitcoin "$BITCOIN_DATA"
+  chown -R groestlcoin:groestlcoin "$(getent passwd groestlcoin | cut -d: -f6)"
+  # Fix permissions for groestlcoin data dir.
+  chown -R groestlcoin:groestlcoin "$GROESTLCOIN_DATA"
 
-  echo "$0: setting data directory to $BITCOIN_DATA"
+  echo "$0: setting data directory to $GROESTLCOIN_DATA"
 
-  set -- "$@" -datadir="$BITCOIN_DATA"
+  set -- "$@" -datadir="$GROESTLCOIN_DATA"
 fi
 
-if [ "$1" = "bitcoind" ] || [ "$1" = "bitcoin-cli" ] || [ "$1" = "bitcoin-tx" ]; then
+if [ "$1" = "groestlcoind" ] || [ "$1" = "groestlcoin-cli" ] || [ "$1" = "groestlcoin-tx" ]; then
   echo
-  exec su-exec bitcoin "$@"
+  exec su-exec groestlcoin "$@"
 fi
 
 echo
